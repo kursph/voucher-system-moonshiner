a8be0451c0a45ffee7fdfed031e2f01b1454c104
kursph <kurspahic.haris@gmail.com>
2022-11-07 22:49:38 +0100

    added voucher and voucher type as entities in relation

---
+++ src/Entity/Voucher.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoucherRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoucherRepository::class)]
+class Voucher
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $permanent = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $validFrom = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $validUntil = null;
+
+    #[ORM\ManyToOne(inversedBy: 'vouchers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?VoucherType $type = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function isPermanent(): ?bool
+    {
+        return $this->permanent;
+    }
+
+    public function setPermanent(?bool $permanent): self
+    {
+        $this->permanent = $permanent;
+
+        return $this;
+    }
+
+    public function getValidFrom(): ?\DateTimeInterface
+    {
+        return $this->validFrom;
+    }
+
+    public function setValidFrom(?\DateTimeInterface $validFrom): self
+    {
+        $this->validFrom = $validFrom;
+
+        return $this;
+    }
+
+    public function getValidUntil(): ?\DateTimeInterface
+    {
+        return $this->validUntil;
+    }
+
+    public function setValidUntil(\DateTimeInterface $validUntil): self
+    {
+        $this->validUntil = $validUntil;
+
+        return $this;
+    }
+
+    public function getType(): ?VoucherType
+    {
+        return $this->type;
+    }
+
+    public function setType(?VoucherType $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
